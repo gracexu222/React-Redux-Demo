ca2d3944b486f94330cf4b89d63ed9a9c92cdb84
Grace Xu <xuweijia222@gmail.com>
2024-12-29 18:56:04 -0800

    modified functions

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -5,14 +5,19 @@ import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
